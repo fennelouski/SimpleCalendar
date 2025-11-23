3c5073eac536f4d6422b181d8cff4a943e7730ee
fennelouski <nathanfennel@gmail.com>
2025-11-23 07:44:14 -0700

    Initial Commit

---
+++ Simple CalendarTests/Simple_CalendarTests.swift
@@ -0,0 +1,16 @@
+//
+//  Simple_CalendarTests.swift
+//  Simple CalendarTests
+//
+//  Created by Nathan Fennel on 11/23/25.
+//
+
+import Testing
+
+struct Simple_CalendarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
